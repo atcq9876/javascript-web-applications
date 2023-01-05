e254ea382a3e2957240c4896d441462a6dc5c817
Andy <ajscottlewis@gmail.com>
2023-01-05 11:51:54 +0000

    Complete fetch exercise

--- phase-three/example-4/githubView.js
+++ phase-three/example-4/githubView.js
@@ -2,6 +2,9 @@ class GithubView {
   constructor(model, client) {
     this.model = model;
     this.client = client;
+    this.repoImageEl = document.createElement('img');
+
+    document.querySelector('div').append(this.repoImageEl);
 
     const submitButtonEl = document.querySelector('#submit-button');
     const repoInputEl = document.querySelector('#repo-name-input');
@@ -10,13 +13,20 @@ class GithubView {
       const repoName = repoInputEl.value;
 
       this.client.getRepoInfo(repoName, repoData => {
+        
         console.log(repoData);
+        this.display(repoData);
       });
     });
   }
 
-  display() {
-
+  display(repoData) {
+    const repoNameEl = document.querySelector('#repo-name');
+    const repoDescriptionEl = document.querySelector('#repo-description');
+    
+    this.repoImageEl.setAttribute('src', repoData.organization.avatar_url);
+    repoNameEl.textContent = repoData.full_name;
+    repoDescriptionEl.textContent = repoData.description;
   }
 }
 
